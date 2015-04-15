8f5b6c91aaec09a2b96692b540ad7f20fa7d8d62
Harry Terkelsen <harry.terkelsen@gmail.com>
2015-04-15 09:26:50 -0700

    Add line wrapping hook in org mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -61,3 +61,9 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
+(setq org-agenda-files (list "~/org/work.org"
+                             "~/org/home.org"))
+;; Set auto-fill for org-mode
+(add-hook 'org-mode-hook (lambda ()
+  (auto-fill-mode)
+  (set-fill-column 66)))
