61f20c7bfbdd675f334affa6f196cc12805c50b3
Harry Terkelsen <het@google.com>
2016-01-19 18:18:44 -0800

    add scripts

--- install.sh
+++ install.sh
@@ -8,4 +8,9 @@ ln -s $DOTFILES/.zshrc $HOME/.zshrc
 ln -s $DOTFILES/.git.zsh $HOME/.git.zsh
 ln -s $DOTFILES/.dev.zsh $HOME/.dev.zsh
 
+mkdir -p $HOME/bin
+ln -s $DOTFILES/bin/dart2jsd $HOME/bin/dart2jsd
+ln -s $DOTFILES/bin/dart2jsd-fast $HOME/bin/dart2jsd-fast
+ln -s $DOTFILES/bin/d8d $HOME/bin/d8d
+
 popd >/dev/null
