509da707ec9fef0ff08624d0c3e7afe3be9b2000
Harry Terkelsen <het@google.com>
2016-05-13 08:35:34 -0700

    add some more git stuffs

--- .git.zsh
+++ .git.zsh
@@ -3,6 +3,7 @@ alias gcl='git clone --recursive'
 
 alias ga='git add'
 alias gaa='git add --all'
+alias gaap='git add --all -p'
 
 alias gb='git branch'
 alias gba='git branch -a'
@@ -15,6 +16,7 @@ alias gcb='git checkout -b'
 alias gcm='git checkout master'
 
 alias gd='git diff'
+alias gdc='git diff --cached'
 
 alias gl='git pull'
 
